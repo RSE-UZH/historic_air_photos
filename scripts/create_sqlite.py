d4eeb5439827993978509a6d0bbd3d16b2831292
Bob McNabb <robertmcnabb@gmail.com>
2024-11-20 00:25:31 +0000

    finish updating sqlite script

--- scripts/create_sqlite.py
+++ scripts/create_sqlite.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sqlite3
 from shapely.geometry import Polygon
 import tools
@@ -39,14 +40,14 @@ db_conn.execute(
 db_conn.execute(
     """
     CREATE TABLE geographic (
-        GeoID TEXT NOT NULL,
+        GeoKey TEXT NOT NULL,
         DatasetKey TEXT NOT NULL,
         PubKey TEXT NOT NULL,
-        Geometry BLOB,
+        Geom BLOB,
         Area REAL,
-        Country TEXT,
+        Region TEXT,
         Notes TEXT,
-        PRIMARY KEY(GeoID),
+        PRIMARY KEY(GeoKey),
         FOREIGN KEY(PubKey) REFERENCES publications(PubKey),
         FOREIGN KEY(DatasetKey) REFERENCES datasets(DatasetKey)
         );
@@ -94,7 +95,7 @@ db_conn.execute(
         NumImgs INTEGER,
         CameraType TEXT,
         Notes TEXT,
-        PRIMARY KEY(DataKey),
+        PRIMARY KEY(DatasetKey),
         FOREIGN KEY(PubKey) REFERENCES publications(PubKey)
         );
     """
@@ -154,7 +155,7 @@ db_conn.execute(
         CPResidualsXYZ REAL,
         CPResiduals REAL,
         Comparison TEXT,
-        ComparisonGroup TEXT,
+        CompGroup TEXT,
         CompAccuracyX REAL,
         CompAccuracyY REAL,
         CompAccuracyZ REAL,
@@ -172,7 +173,7 @@ db_conn.execute(
         Notes TEXT,
         PRIMARY KEY(AccuracyKey),
         FOREIGN KEY(PubKey) REFERENCES publications(PubKey),
-        FOREIGN KEY(DatasetKey) REFERENCES publications(DatasetKey),
+        FOREIGN KEY(DatasetKey) REFERENCES publications(DatasetKey)
         );
     """
 )
@@ -189,7 +190,7 @@ db_conn.execute(
         Note TEXT,
         PRIMARY KEY(DatasetKey),
         FOREIGN KEY(PubKey) REFERENCES publications(PubKey),
-        FOREIGN KEY(DatasetKey) REFERENCES publications(DatasetKey),
+        FOREIGN KEY(DatasetKey) REFERENCES publications(DatasetKey)
         );
     """
 )
@@ -207,7 +208,8 @@ db_conn.execute(
         EndYear INTEGER,
         URL TEXT,
         GeoURL TEXT,
-        PRIMARY KEY(ArchiveKey),
+        Notes TEXT,
+        PRIMARY KEY(ArchiveKey)
         );
     """
 )
@@ -216,76 +218,130 @@ db_conn.execute(
 # we also re-name columns along the way
 
 # populate the publications table
-pubs.rename(mapper={'Key': 'id', 'Publication Title': 'PubTitle', '.not_relevant': 'Relevant'},
-            axis='columns', inplace=True)
-pubs_cols = ['id', 'Author', 'Year', 'Title', 'PubTitle', 'DOI', 'Relevant']
+pubs = pubs.merge(sci[['PubKey', 'Relevant']], left_on='PubKey', right_on='PubKey')
+pubs.rename(columns={'Publication Title': 'PubTitle'},
+            inplace=True)
+pubs_cols = ['PubKey', 'Author', 'Year', 'Title', 'PubTitle', 'DOI', 'Relevant']
 pubs[pubs_cols].to_sql('publications', db_conn, if_exists='append', index=False)
 
 # populate the geographic table
-geog['GeoID'] = geog.index
-geog['PubID'] = geog['Publication Key'].str.extract(r'\((.*?)\)')
+geog = tools.expand_study_areas(geog, data)
+
+counts = dict(zip(list(geog['PubKey'].unique()),
+                  np.zeros(len(geog['PubKey'].unique()))))
+
+for ind, row in geog.sort_values('DatasetKey').iterrows():
+    geog.loc[ind, 'GeoKey'] = row['PubKey'] + '.G' + str(int(counts[row['PubKey']] + 1))
+    counts[row['PubKey']] += 1
 
 geog.dropna(subset=['lat_min', 'lat_max', 'lon_min', 'lon_max'], inplace=True)
-geog['Geom'] = [Polygon(_coords(row)).wkb for ii, row in geog.iterrows()]
+geog.rename(columns={'Geographic Notes': 'Notes'}, inplace=True)
 
-geo_cols = ['GeoID', 'Geom', 'Notes', 'PubID']
+geog['Geom'] = [Polygon(_coords(row)).wkb for _, row in geog.iterrows()]
+
+geo_cols = ['GeoKey', 'DatasetKey', 'PubKey', 'Geom', 'Area', 'Region', 'Notes']
 geog[geo_cols].to_sql('geographic', db_conn, if_exists='append', index=False)
 
 db_conn.execute("UPDATE geographic SET Geometry = CastToMultiPolygon(ST_GeomFromWKB(Geom, 4326));")
 
 # populate the scientific table
-sci['PubID'] = sci['Publication Key'].str.extract(r'\((.*?)\)')
-sci.rename(mapper={'Data Type': 'DataType', 'Type of Study': 'StudyType'}, axis='columns', inplace=True)
+sci.rename(columns={'Data Type': 'DataType', 'Type of Study': 'StudyType',
+                    'Study Description': 'Description', 'Scientific Notes': 'Notes'}, inplace=True)
 
-sci_cols = ['PubID', 'DataType', 'StudyType', 'Category', 'Relevant', 'Description', 'Notes']
+sci_cols = ['PubKey', 'DataType', 'StudyType', 'Category', 'Description', 'Notes']
 sci[sci_cols].to_sql('scientific', db_conn, if_exists='append', index=False)
 
 # populate the dataset table
 data[['PubID', 'Num']] = outs['Key'].str.split('.', expand=True)
-data.rename(mapper={'Key': 'DataID', 'Archive Location': 'Location', 'Freely Available?': 'Free',
-                    'Archive Name': 'ArchiveName', 'Acquisition Start Year': 'StartYear',
-                    'Acquisition End Year': 'EndYear', 'Camera calib?': 'Calibration',
-                    'Flight Height [m]': 'FlightHeight', 'Height reference': 'HeightRef',
-                    'GSD [m]': 'GSD', 'Scanner resolution': 'ScanRes', 'Scanner resolution units': 'ScanUnit',
-                    'No. Images': 'NumImgs'}, axis='columns', inplace=True)
+data.rename(columns={'Key': 'DatasetKey', 'Freely Available?': 'Free',
+                     'Acquisition Start Year': 'StartYear',
+                     'Acquisition End Year': 'EndYear', 'Camera Calibration': 'Calibration',
+                     'Flight Height [m]': 'FlightHeight', 'Height reference': 'HeightRef',
+                     'GSD [m]': 'GSD', 'Scanner resolution': 'ScanRes', 'Scanner resolution units': 'ScanUnit',
+                     'No. Images': 'NumImgs', 'Camera type': 'CameraType',
+                     'Dataset Notes': 'Notes'}, inplace=True)
 
-data_cols = ['PubID', 'DataID', 'Type', 'Location', 'Free', 'ArchiveName', 'StartYear', 'EndYear',
+data_cols = ['DatasetKey', 'PubKey', 'Type', 'ArchiveKey', 'Free', 'StartYear', 'EndYear',
              'Calibration', 'FlightHeight', 'HeightRef', 'GSD', 'Scale', 'ScanRes', 'ScanUnit',
-             'NumImgs', 'Notes']
+             'NumImgs', 'CameraType', 'Notes']
 data[data_cols].to_sql('datasets', db_conn, if_exists='append', index=False)
 
 # populate the processing table
-proc[['PubID', 'Num']] = outs['Key'].str.split('.', expand=True)
-proc.rename(mapper={'Key': 'DataID', 'Fiducial Marks': 'Fiducial', 'Pre-processing': 'PreProc',
-                    'Pre-processing Note': 'PreProcNote', 'Workflow Note': 'WorkNote',
-                    'Related paper': 'Related'}, axis='columns', inplace=True)
+proc.rename(columns={'Key': 'DatasetKey', 'Fiducial Marks': 'Fiducial', 'Pre-processing': 'PreProc',
+                     'Simplified Pre-processing Note': 'PreProcNote',
+                     'Geometric Pre-processing': 'Geometric',
+                     'Radiometric Pre-processing': 'Radiometric',
+                     'Workflow Note': 'Workflow',
+                     'Related paper': 'Related'}, inplace=True)
 
-proc_cols = ['PubID', 'DataID', 'Method', 'Software', 'Version', 'GCPs', 'Fiducial',
-             'PreProc', 'PreProcNote', 'WorkNote', 'Related']
+proc_cols = ['DatasetKey', 'PubKey', 'Method', 'Software', 'Version', 'GCPs', 'Fiducial',
+             'PreProc', 'PreProcNote', 'Geometric', 'Radiometric', 'Workflow']
 proc[proc_cols].to_sql('processing', db_conn, if_exists='append', index=False)
 
 # populate the accuracy table
-acc[['PubID', 'Num']] = outs['Key'].str.split('.', expand=True)
-acc.rename(mapper={'Key': 'DataID', 'Ground control source XY': 'SourceXY', 'Ground control source Z': 'SourceZ',
-                   'Ground control accuracy [m] XY': 'AccuracyXY', 'Ground control accuracy [m] Z': 'AccuracyZ',
-                   'No. GCPs': 'NumGCPs', 'Residuals to GCPs [m] XY': 'XYRes', 'Residuals to GCPs [m] Z': 'ZRes', 
-                   'Comparison data': 'Comparison', 'Accuracy comparison data [m] XY': 'CompAccXY',
-                   'Accuracy comparison data [m] Z': 'CompAccZ', 'Residuals to comparison [m] XY': 'CompResXY',
-                   'Residuals to comparison [m] Z': 'CompResZ', 'comparison metric': 'Metric',
-                   'Post-Processing': 'PostProc'}, axis='columns', inplace=True)
+# first, calculate the "avg" for the different metrics
+acc = tools.accuracy_measures(acc)
+acc.rename(columns={'Accuracy Key': 'AccuracyKey',
+                    'Ground control source XY': 'SourceXY',
+                    'Ground control source Z': 'SourceZ',
+                    'Ground control source group': 'SourceGroup',
+                    'Ground control accuracy [m] X': 'SourceAccuracyX',
+                    'Ground control accuracy [m] Y': 'SourceAccuracyY',
+                    'Ground control accuracy [m] Z': 'SourceAccuracyZ',
+                    'Ground control accuracy [m] XY': 'SourceAccuracyXY',
+                    'Ground control accuracy [m] XYZ': 'SourceAccuracyXYZ',
+                    'Ground control accuracy [m] avg': 'SourceAccuracy',
+                    'No. GCPs': 'NumGCPs',
+                    'Residuals to GCPs [m] X': 'GCPResidualsX',
+                    'Residuals to GCPs [m] Y': 'GCPResidualsY',
+                    'Residuals to GCPs [m] Z': 'GCPResidualsZ',
+                    'Residuals to GCPs [m] XY': 'GCPResidualsXY',
+                    'Residuals to GCPs [m] XYZ': 'GCPResidualsXYZ',
+                    'Residuals to GCPs [m] avg': 'GCPResiduals',
+                    'No. CPs': 'NumCPs',
+                    'Residuals to CPs [m] X': 'CPResidualsX',
+                    'Residuals to CPs [m] Y': 'CPResidualsY',
+                    'Residuals to CPs [m] Z': 'CPResidualsZ',
+                    'Residuals to CPs [m] XY': 'CPResidualsXY',
+                    'Residuals to CPs [m] XYZ': 'CPResidualsXYZ',
+                    'Residuals to CPs [m] avg': 'CPResiduals',
+                    'Comparison data': 'Comparison',
+                    'Comparison source group': 'CompGroup',
+                    'Accuracy comparison data [m] X': 'CompAccuracyX',
+                    'Accuracy comparison data [m] Y': 'CompAccuracyY',
+                    'Accuracy comparison data [m] Z': 'CompAccuracyZ',
+                    'Accuracy comparison data [m] XY': 'CompAccuracyXY',
+                    'Accuracy comparison data [m] XYZ': 'CompAccuracyXYZ',
+                    'Accuracy comparison data [m] avg': 'CompAccuracy',
+                    'Residuals to comparison [m] X': 'CompResidualsX',
+                    'Residuals to comparison [m] Y': 'CompResidualsY',
+                    'Residuals to comparison [m] Z': 'CompResidualsZ',
+                    'Residuals to comparison [m] XY': 'CompResidualsXY',
+                    'Residuals to comparison [m] XYZ': 'CompResidualsXYZ',
+                    'Residuals to comparison [m] avg': 'CompResiduals',
+                    'Comparison Metric': 'Metric',
+                    'Post-Processing': 'PostProcessing'}, inplace=True)
 
-acc_cols = ['PubID', 'DataID', 'SourceXY', 'SourceZ', 'AccuracyXY', 'AccuracyZ', 'NumGCPs',
-            'XYRes', 'ZRes', 'Comparison', 'CompAccXY', 'CompAccZ', 'Metric', 'PostProc', 'Notes']
+acc_cols = ['AccuracyKey', 'DatasetKey', 'PubKey', 'SourceXY', 'SourceZ', 'SourceGroup',
+            'SourceAccuracyX', 'SourceAccuracyY', 'SourceAccuracyZ', 'SourceAccuracyXY', 'SourceAccuracyXYZ',
+            'SourceAccuracy', 'NumGCPs', 'GCPResidualsX', 'GCPResidualsY', 'GCPResidualsZ', 'GCPResidualsXY',
+            'GCPResidualsXYZ', 'GCPResiduals', 'NumCPs', 'CPResidualsX', 'CPResidualsY', 'CPResidualsZ',
+            'CPResidualsXY', 'CPResidualsXYZ', 'CPResiduals', 'Comparison', 'CompGroup', 'CompAccuracyX',
+            'CompAccuracyY', 'CompAccuracyZ', 'CompAccuracyXY', 'CompAccuracyXYZ', 'CompAccuracy',
+            'CompResidualsX', 'CompResidualsY', 'CompResidualsZ', 'CompResidualsXY', 'CompResidualsXYZ',
+            'CompResiduals', 'Metric', 'PostProcessing']
 acc[acc_cols].to_sql('accuracy', db_conn, if_exists='append', index=False)
 
 # populate the outputs table
 outs[['PubID', 'Num']] = outs['Key'].str.split('.', expand=True)
-outs.rename(mapper={'Key': 'DataID', 'DEM resolution [m]': 'DEMRes',
-                    'Orthophoto resolution [m]': 'OrthoRes', 'note': 'Note'}, axis='columns', inplace=True)
+outs.rename(mapper={'Key': 'DatasetKey', 'DEM resolution [m]': 'DEMRes',
+                    'Orthophoto resolution [m]': 'OrthoRes', 'Output Notes': 'Note'}, axis='columns', inplace=True)
 
-outs_cols = ['PubID', 'DataID', 'Output', 'OrthoRes', 'DEMRes', 'Note']
+outs_cols = ['DatasetKey', 'PubKey', 'Output', 'OrthoRes', 'DEMRes', 'Note']
 outs[outs_cols].to_sql('outputs', db_conn, if_exists='append', index=False)
+
+# populate the archives table
+archs.to_sql('archives', db_conn, if_exists='append', index=False)
 
 # close the connection to the database
 db_conn.close()
-
